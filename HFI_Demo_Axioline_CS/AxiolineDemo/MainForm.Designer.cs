95ae99430700d231a90d04f49940619d2d440533
aleawgn <ale_880108@hotmail.it>
2020-04-09 13:51:51 +0200

    esempi Phoenix

---
+++ HFI_Demo_Axioline_CS/AxiolineDemo/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace HFI_Demo_Axioline_CS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.axlDeviceCtrl1 = new PhoenixContact.HFI.Axioline.WinFormComponents.AxlDeviceCtrl();
+            this.axlControllerCtrl1 = new PhoenixContact.HFI.Axioline.WinFormComponents.AxlControllerCtrl();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // axlDeviceCtrl1
+            // 
+            this.axlDeviceCtrl1.ControlText = "AxlDeviceCtrl";
+            this.axlDeviceCtrl1.EditActivate = false;
+            this.axlDeviceCtrl1.Location = new System.Drawing.Point(12, 333);
+            this.axlDeviceCtrl1.Name = "axlDeviceCtrl1";
+            this.axlDeviceCtrl1.Size = new System.Drawing.Size(755, 371);
+            this.axlDeviceCtrl1.TabIndex = 0;
+            this.axlDeviceCtrl1.Load += new System.EventHandler(this.axlDeviceCtrl1_Load);
+            // 
+            // axlControllerCtrl1
+            // 
+            this.axlControllerCtrl1.ControlText = "IAxlControllerCtrl";
+            this.axlControllerCtrl1.Location = new System.Drawing.Point(12, 12);
+            this.axlControllerCtrl1.Name = "axlControllerCtrl1";
+            this.axlControllerCtrl1.Size = new System.Drawing.Size(755, 315);
+            this.axlControllerCtrl1.TabIndex = 1;
+            this.axlControllerCtrl1.Load += new System.EventHandler(this.axlControllerCtrl1_Load);
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Tick += new System.EventHandler(this.UpdateTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 713);
+            this.Controls.Add(this.axlControllerCtrl1);
+            this.Controls.Add(this.axlDeviceCtrl1);
+            this.Name = "MainForm";
+            this.Text = "HFI Demo Axioline C#";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PhoenixContact.HFI.Axioline.WinFormComponents.AxlDeviceCtrl axlDeviceCtrl1;
+        private PhoenixContact.HFI.Axioline.WinFormComponents.AxlControllerCtrl axlControllerCtrl1;
+        private System.Windows.Forms.Timer updateTimer;
+    }
+}
---
+++ HFI_Demo_Axioline_CS/AxiolineDemo/MainForm.cs
@@ -0,0 +1,140 @@
+﻿#region Copyright
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright PHOENIX CONTACT Software GmbH
+//
+///////////////////////////////////////////////////////////////////////////////
+#endregion
+
+namespace HFI_Demo_Axioline_CS
+{
+    using System;
+    using System.Windows.Forms;
+
+    using PhoenixContact.HFI.Axioline;
+    using PhoenixContact.HFI.Axioline.AxlDevices;
+    using PhoenixContact.PxC_Library.Util;
+
+    public partial class MainForm : Form
+    {
+        #region *** Global Exception Handling *******************************************
+
+        /// <summary>
+        /// Different error types.
+        /// </summary>
+        private enum ErrorType
+        {
+            Unknown = 0,
+            Application = 1,        // possible Application errors from 1 to 29999
+            Domain = 30000          // possible Domain errors from 30000 to int.MaxValue
+        }
+
+        /// <summary>
+        /// Handle the application exception.
+        /// </summary>
+        /// <param name="sender">The object with the exception.</param>
+        /// <param name="e">The exception.</param>
+        private void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            this.ShowError(ErrorType.Application, e.Exception);
+        }
+
+        /// <summary>
+        /// Handle the application domain exception.
+        /// </summary>
+        /// <param name="sender">The object with the exception.</param>
+        /// <param name="e">The exception.</param>
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            this.ShowError(ErrorType.Domain, (Exception)e.ExceptionObject);
+        }
+
+        /// <summary>
+        /// Show the error message.
+        /// </summary>
+        /// <param name="type">The error type.</param>
+        /// <param name="e">The exception.</param>
+        private void ShowError(ErrorType type, Exception e)
+        {
+            // Fehlermeldung beim öffnen der Datei
+            MessageBox.Show(
+                "Error Source = " + type.ToString() + Environment.NewLine + Environment.NewLine
+                + EnvironmentInfo.GetAllInformation(e),
+                Application.ProductName);
+
+            // Exception behavior
+            Application.Exit();
+        }
+
+        #endregion *** Global Exception Handling *********************************************
+
+        private readonly AxlBkApplication myApplication;
+        
+        public MainForm()
+        {
+            // This two events catch all unhandled exceptions.
+            Application.ThreadException += this.Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += this.CurrentDomain_UnhandledException;
+
+            this.InitializeComponent();
+
+            // Create a new instance from the application class.
+            this.myApplication = new AxlBkApplication();
+          
+            // Register an event if the buscoupler changes.
+            this.axlControllerCtrl1.AfterSelectController += this.AxlControllerCtrl1_AfterSelectController; 
+
+            // Add the buscoupler to the control.
+            this.axlControllerCtrl1.AddObject(this.myApplication.Buscoupler);
+        }
+
+        private void AxlControllerCtrl1_AfterSelectController(object sender, object controller)
+        {
+            this.AddDevicesToControl(controller as IAxlController);         
+        }
+
+        /// <summary>
+        /// Add the devices from the buscoupler to the control.
+        /// </summary>
+        /// <param name="controller">The selected controller.</param>
+        private void AddDevicesToControl(IAxlController controller)
+        {
+            if (controller != null)
+            {
+                // Delete old devices
+                this.axlDeviceCtrl1.ClearAllObjects();
+                
+                // Add the controller devices to the device control.
+                foreach (AxlDevice i in controller.DeviceList)
+                {
+                    this.axlDeviceCtrl1.AddObject(i);
+                }         
+            }
+        }
+              
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.myApplication != null)
+            {
+                this.myApplication.Dispose();
+            }
+        }
+
+        private void UpdateTimer_Tick(object sender, EventArgs e)
+        {
+            // Update the controls for the Axioline buscoupler and the Axioline devices.
+            this.axlControllerCtrl1.UpdateData();
+            this.axlDeviceCtrl1.UpdateData();
+        }
+
+        private void axlControllerCtrl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axlDeviceCtrl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
